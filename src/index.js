31beff1fc778bc8c3870be3a60c7ff1e9745bd41
Gassyr <gassyrdaulet@gmail.com>
2023-12-10 00:39:17 +0600

    a

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { app } from "./config/firebase.js";
+import "./config/firebase.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(app.name);
 
 root.render(
   <BrowserRouter>
